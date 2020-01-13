a7a8ddb6a540ce8561db1b87f357853dc14832ad
Sebastian Springer <info@sebastian-springer.com>
2020-01-13 11:58:19 +0100

    cleanup and integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [TodoController],
+  providers: [TodoService],
 })
 export class AppModule {}
