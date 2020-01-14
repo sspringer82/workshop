6e5aeaa90199b0ecc23e17174e64d34979424abb
Sebastian Springer <info@sebastian-springer.com>
2020-01-14 14:09:37 +0100

    class component

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
-import Name from './Name';
 import Todo from './Todo';
 import { Todo as TodoType } from './Todo.interface';
+import Lights from './Lights';
 
 const App: React.FC = () => {
+  console.log('App render');
   const todo: TodoType = {
     id: '1',
     title: 'Get up',
     done: false,
-  };
-
-  const handleClick = (name: string) => {
-    alert('you clicked the ' + name);
   };
 
   const handleStatusChange = (todo: TodoType) => {
@@ -20,8 +17,8 @@ const App: React.FC = () => {
 
   return (
     <>
+      <Lights />
       <Todo onStatusChange={handleStatusChange} todo={todo} />
-      <Name name="Basti" onClick={handleClick} />
     </>
   );
 };
---
+++ todo/src/Lights.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+interface State {
+  canWalk: boolean;
+}
+
+interface Props {}
+
+export default class Lights extends React.Component<Props, State> {
+  state = {
+    canWalk: false,
+  };
+
+  render() {
+    const values = ['A', 'B', 'C'];
+
+    console.log('Lights render');
+
+    const handleClick = () => {
+      //this.setState({ canWalk: !this.state.canWalk });
+
+      this.setState(prevState => {
+        return { ...prevState, canWalk: !prevState.canWalk };
+      });
+    };
+
+    return (
+      <div style={{ border: '1px solid black', marginBottom: '20px' }}>
+        <div>{this.state.canWalk ? 'walk' : 'stop'}</div>
+        <button onClick={handleClick}>toggle</button>
+        {values.map(v => (
+          <div>{v}</div>
+        ))}
+      </div>
+    );
+  }
+}
