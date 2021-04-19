ecd653643c87d1db0d23334bdd53819249056f0d
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-04-19 10:52:58 +0200

    base recipe structure

--- src/demo/component.tsx
+++ src/demo/component.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+const recipes = [
+  {
+    id: 1,
+    title: 'Schnitzel',
+    ingredients: ['1000g Fleisch'],
+    steps: ['braten'],
+  },
+  {
+    id: 2,
+    title: 'Tomatensuppe',
+    ingredients: ['1000g Tomaten', '1l Wasser'],
+    steps: ['kochen', 'umrühren'],
+  },
+];
+
 const Component = () => {
   const a = [1, 2, 3];
 
