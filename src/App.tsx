f0f13dcb89d9ee0b8899248b3a0e2849fdb4fbe8
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2021-04-19 10:15:57 +0200

    minor change

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,6 @@ function getAppName() {
   return 'Kochbuch';
 }
 
-function App1() {
-  return <div>Hallo {getAppName()}</div>;
-}
-
-class App2 extends React.Component {
-  getAppName() {
-    return 'Kochbuch';
-  }
-  render() {
-    return <div>Hallo {this.getAppName()}</div>;
-  }
-}
-
 const App: React.FC = () => {
   return <div>Hallo {getAppName()}</div>;
 };
